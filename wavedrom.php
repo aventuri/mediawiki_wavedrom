c479ba66885b09e595bfe298b936eb6043627df4
Quanticles <dave.fick@gmail.com>
2016-05-24 16:55:27 -0500

    Updated to support Mediawiki 1.26.2

--- wavedrom.php
+++ wavedrom.php
@@ -1,23 +1,22 @@
 <?php
-
-$wgHooks['ParserFirstCallInit'][] = 'ExampleExtension::onParserSetup';
-
-class ExampleExtension {
-	// Register any render callbacks with the parser
-	function onParserSetup( Parser $parser ) {
-		// When the parser sees the <wavedrom> tag, it executes renderTagWavedrom (see below)
-		$parser->setHook( 'wavedrom', 'ExampleExtension::renderTagWavedrom' );
-	}
-
-	// Render <wavedrom>
+$wgHooks['ParserFirstCallInit'][] = 'WaveDrom::onParserSetup';
+class WaveDrom {
+      // Register any render callbacks with the parser
+      function onParserSetup( Parser $parser ) {
+               // When the parser sees the <wavedrom> tag, it executes renderTagWavedrom (see below)
+                  $parser->setHook( 'wavedrom', 'WaveDrom::renderTagWavedrom' );
+                  }
+                  // Render <wavedrom>
   function renderTagWavedrom( $input, array $args, Parser $parser, PPFrame $frame ) {
-    $includeSkin = '<script src="http://wavedrom.com/skins/default.js" type="text/javascript"></script>';
-    $includeWavedom = '<script src="http://wavedrom.com/wavedrom.min.js" type="text/javascript"></script>';
+    //$parser->addHeadItem(
+    $parser->getOutput()->addHeadItem('<script src="http://wavedrom.com/skins/default.js" type="text/javascript"></script>', 'WaveDromDefault');
+    $parser->getOutput()->addHeadItem('<script src="http://wavedrom.com/wavedrom.min.js" type="text/javascript"></script>', 'WaveDromMain');
+    $parser->getOutput()->addHeadItem('<script>window.addEventListener(\'DOMContentLoaded\', WaveDrom.ProcessAll, false);</script>', 'WaveDromLoad');
     // Mediawiki add <p> and <pre> if newlines ...
     $inputstriped = str_replace(array("\n", "\r"), "", $input);
-    $thescript = '<script type="WaveDrom">'.$inputstriped.'</script>';
-		return $includeSkin . $includeWavedom . $thescript;
-	}
+    return '<script type="WaveDrom">'.$inputstriped.'</script>';
+  }
 }
-
 ?>
+
+
