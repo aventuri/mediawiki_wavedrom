cd37b2f38cbc4e70c9956c7f4a1754eafba553b6
andrea venturi <be17068@iperbole.bo.it>
2024-07-11 12:18:29 +0200

    Update to wavedrom backend ver 3.5.0

--- wavedrom.php
+++ wavedrom.php
@@ -9,8 +9,8 @@ class WaveDrom {
                   // Render <wavedrom>
   public static function renderTagWavedrom( $input, array $args, Parser $parser, PPFrame $frame ) {
     //$parser->addHeadItem(
-    $parser->getOutput()->addHeadItem('<script src="https://cdnjs.cloudflare.com/ajax/libs/wavedrom/2.6.8/skins/default.js" type="text/javascript"></script>', 'WaveDromDefault');
-    $parser->getOutput()->addHeadItem('<script src="https://cdnjs.cloudflare.com/ajax/libs/wavedrom/2.6.8/wavedrom.min.js" type="text/javascript"></script>', 'WaveDromMain');
+    $parser->getOutput()->addHeadItem('<script src="https://cdnjs.cloudflare.com/ajax/libs/wavedrom/3.5.0/skins/default.js" type="text/javascript"></script>', 'WaveDromDefault');
+    $parser->getOutput()->addHeadItem('<script src="https://cdnjs.cloudflare.com/ajax/libs/wavedrom/3.5.0/wavedrom.min.js" type="text/javascript"></script>', 'WaveDromMain');
     $parser->getOutput()->addHeadItem('<script>window.addEventListener(\'DOMContentLoaded\', WaveDrom.ProcessAll, false);</script>', 'WaveDromLoad');
     // Mediawiki add <p> and <pre> if newlines ...
     $inputstriped = str_replace(array("\n", "\r"), "", $input);
